faed77bbd5ccbaea63d8fa314670b4f327c7352a
JohnSmith <salapin.danil1@mail.ru>
2020-02-25 07:11:12 +0000

    updated script.js

--- src/script.js
+++ src/script.js
@@ -5,10 +5,13 @@ var taskSelector = document.querySelector('.task');
 var answer = document.querySelectorAll('.answer-container');
 var stopAfunction = 0;
 var titleList = ['word 1', 'Word 2', 'Word 3', 'Word 4', 'Word 5', 'Word 6', 'Word 7', 'Word 8', 'Word 9', 'Word 10'];
-var loopOut = 1;
-
-
-
+var testPosition = 1;
+var startTestButton = document.querySelector('.start-button');
+var startTestContainer = document.querySelector('.start-test-container');
+var container = document.querySelector('.main');
+var content = document.querySelector('.container');
+var numberOftask = 0;
+var start = 0;
 
 function QuestionLibrary() {
   this.questions = [
@@ -45,11 +48,45 @@ const library = new QuestionLibrary();
 function Test(questions) {
   this.questions = questions;
   this.status = false;
+  this.rightAnswersSum = 0;
+  this.numberOftask = 0;
+  this.answersBox = [];
   this.run = function() {
     this.status = true;
   };
   this.answer = function(questionId, answer) {
-    this.questions[questionId].answer = answer;
+    if (this.status === true) {
+      this.questions[questionId].answer = answer;
+    }
+  };
+  this.getRandomAnswer = function() {
+    if (this.status === true) {
+      var random1;
+      var random2;
+      var random3;
+      var random4;
+      var numbers = [];
+      while (numbers.length < 4) {
+        var randomNumber = Math.floor(Math.random() * 4);
+        if (numbers.indexOf(randomNumber) === -1) {
+          numbers.push(randomNumber);
+        }
+      }
+      window.random1 = numbers[0];
+      window.random2 = numbers[1];
+      window.random3 = numbers[2];
+      window.random4 = numbers[3];
+    }
+  };
+  this.generateTask = function() {
+    this.getRandomAnswer();
+
+    this.answersBox = [];
+    this.answersBox.push(library.questions[this.numberOftask].answers[random1]);
+    this.answersBox.push(library.questions[this.numberOftask].answers[random2]);
+    this.answersBox.push(library.questions[this.numberOftask].answers[random3]);
+    this.answersBox.push(library.questions[this.numberOftask].answers[random4]);
+    this.numberOftask++;
   }
 }
 
@@ -57,50 +94,25 @@ const questions = library.getRandomQuestions();
 const test = new Test(questions);
 
 
-
-
-
-
-function getRandomAnswer() {
-  var random1;
-  var random2;
-  var random3;
-  var random4;
-  var numbers = [];
-  while (numbers.length < 4) {
-    var randomNumber = Math.floor(Math.random() * 4);
-    if (numbers.indexOf(randomNumber) === -1) {
-      numbers.push(randomNumber);
-    }
-  }
-  window.random1 = numbers[0];
-  window.random2 = numbers[1];
-  window.random3 = numbers[2];
-  window.random4 = numbers[3];
-}
-
-
-function generateTask(title, index) {
-  if (loopOut < 10) {
-    getRandomAnswer();
+function generateTaskUI(title, index) {
+  if (testPosition < 10) {
+    test.generateTask();
     taskSelector.innerHTML = '<h1 class="task-title">' + titleList[title] + ':' + '</h1><span class="task-word">' + library.questions[index].question + '</span>';
-    answer[0].innerHTML = '<div class ="answer">' + library.questions[index].answers[random1] + '</div>';
-    answer[1].innerHTML = '<div class ="answer">' + library.questions[index].answers[random2] + '</div>';
-    answer[2].innerHTML = '<div class ="answer">' + library.questions[index].answers[random3] + '</div>';
-    answer[3].innerHTML = '<div class ="answer">' + library.questions[index].answers[random4] + '</div>';
+    answer[0].innerHTML = '<div class ="answer">' + test.answersBox[0] + '</div>';
+    answer[1].innerHTML = '<div class ="answer">' + test.answersBox[1] + '</div>';
+    answer[2].innerHTML = '<div class ="answer">' + test.answersBox[2] + '</div>';
+    answer[3].innerHTML = '<div class ="answer">' + test.answersBox[3] + '</div>';
   }
-  if (loopOut >= 10) {
+  if (testPosition >= 10) {
+    container.classList.add('main__hidden');
     taskSelector.innerHTML = '<h1 class="task-title">' + 'total' + ':' + '</h1>';
-    var box = document.querySelector('.answers-box');
-    box.innerHTML = '<span class="task-word">' + 'Right answers: ' + test1.rightAnswersSum + '</span>';
+    var resultBox = document.createElement('div');
+    resultBox.className = 'start-test-container';
+    resultBox.innerHTML = '<span class="task-word">' + 'Right answers: ' + test.rightAnswersSum + '</span>';
+    content.appendChild(resultBox);
   }
 }
 
-
-var numberOftask = 0;
-
-
-var start = 0;
 
 function progress() {
   start += 10;
@@ -117,18 +129,20 @@ var userWrong = false;
 var pass = false;
 
 function showRightAnswer(autoShow) {
-  for (var i = 0; i < answer.length; i++) {
-    if (answer[i].innerText === library.questions[numberOftask].correct) {
-      answer[i].classList.add('answer-container_right');
-      checked = true;
-      timeLock = true;
-      if (!autoShow && userWrong === false) {
-        test.rightAnswersSum++;
-        userWrong = true;
+  if (testPosition <= 10) {
+    for (var i = 0; i < answer.length; i++) {
+      if (answer[i].innerText === library.questions[numberOftask].correct) {
+        answer[i].classList.add('answer-container_right');
+        checked = true;
+        timeLock = true;
+        if (!autoShow && userWrong === false) {
+          test.rightAnswersSum++;
+          userWrong = true;
+        }
       }
     }
+    pass = true;
   }
-  pass = true;
 }
 
 function showWrongAnswer(e) {
@@ -168,10 +182,22 @@ function isAnswerRight() {
 }
 
 function updateUi() {
-  generateTask(numberOftask, numberOftask);
-  getRandomAnswer();
+  generateTaskUI(numberOftask, numberOftask);
+  test.getRandomAnswer();
   isAnswerRight();
 }
+
+
+function startTest(testName) {
+  testName.run();
+  updateUi();
+  timeOut();
+  startTestContainer.classList.add('start-test-container__hidden');
+  container.classList.remove('main__hidden');
+}
+startTestButton.addEventListener('click', function() {
+  startTest(test);
+});
 
 
 function timeOut(time) {
@@ -199,8 +225,6 @@ function timeOut(time) {
 var button = document.querySelector('.button');
 
 
-
-
 button.addEventListener('click', function() {
   if (pass === true) {
     cleanAnswers();
@@ -209,14 +233,10 @@ button.addEventListener('click', function() {
     timeOut();
     progress();
     updateUi();
-    loopOut += 1;
+    testPosition += 1;
     pass = false;
   }
 });
-timeOut();
-updateUi();
-
-
 
 /*
   function progress(time) {
